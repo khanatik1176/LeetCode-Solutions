08a561a8693f9af872cebbb65648df290efad554
Khan Atik Faisal <khanatik1176@gmail.com>
2024-10-31 18:08:21 +0600

    MissingNumbr-Array

---
+++ Easy/MissingNumber/app.js
@@ -0,0 +1,31 @@
+var missingNumber = function(nums) {
+
+    let totalNumbers = nums.length;
+
+    let countHashtable = {}
+
+    for(let i=0; i<=totalNumbers; i++)
+    {
+        countHashtable[i] = 0;
+    }
+
+    for(let j=0; j<=totalNumbers; j++)
+    {
+        if(countHashtable.hasOwnProperty(nums[j]))
+        {
+            countHashtable[nums[j]]++;
+        }
+    }
+ 
+    for(let keys in countHashtable)
+    {
+        if(countHashtable[keys] == 0)
+        {
+            let missingValue = keys;
+            return parseInt(missingValue);
+        }
+    }
+
+};
+
+console.log(missingNumber([3,0,1]));
